8aa19a251410cc5191a148756000df8955212594
Kiran Srinivasan <80209239+ghadilion@users.noreply.github.com>
2022-10-06 23:55:12 +0530

    Added lose condition to python implementation

--- 2048.py
+++ 2048.py
@@ -1,112 +1,107 @@
 from random import random, randint
 
-def createBoard():
-    row = [0]*4
-    board = [row[:] for i in range(4)]
+class Game_2048:
+    def __init__(self):
+        row = [0]*4
+        self.board = [row[:] for i in range(4)]
 
-    loc1 = [randint(0,3), randint(0,3)]
-    loc2 = [randint(0,3), randint(0,3)]
-
-    while loc2 == loc1:
+        loc1 = [randint(0,3), randint(0,3)]
         loc2 = [randint(0,3), randint(0,3)]
 
-    if random() <= 0.1:
-        board[loc1[0]][loc1[1]] = 4
-    else:
-        board[loc1[0]][loc1[1]] = 2
+        while loc2 == loc1:
+            loc2 = [randint(0,3), randint(0,3)]
 
-    if random() <= 0.1:
-        board[loc2[0]][loc2[1]] = 4
-    else:
-        board[loc2[0]][loc2[1]] = 2
+        if random() <= 0.1:
+            self.board[loc1[0]][loc1[1]] = 4
+        else:
+            self.board[loc1[0]][loc1[1]] = 2
 
-    return board
+        if random() <= 0.1:
+            self.board[loc2[0]][loc2[1]] = 4
+        else:
+            self.board[loc2[0]][loc2[1]] = 2
     
-def game(board):
-    while not any(2048 in row for row in board):
-        board = turn(board)
-        print('\n', *board, sep='\n')
+    def game(self):
+        print(*self.board,sep='\n')
+        while True:
+            self.turn()
 
-def insertNewNumber(board):
-    loc = [randint(0,3), randint(0,3)]
-    while board[loc[0]][loc[1]] != 0:
-        loc = [randint(0,3), randint(0,3)]
-    if random() <= 0.1:
-        board[loc[0]][loc[1]] = 4
-    else:
-        board[loc[0]][loc[1]] = 2
-    return board
+            if self.board == 0:
+                print('\nYou lose!')
+                break
+            print('\n', *self.board, sep='\n')
 
-def turn(board):
-    move = str(input('Enter move: '))
-    changed = False
-    while not changed:
-        if move == 's':
-            board, changed, = move_down(board)    
-        elif move == 'w':
-            board, changed = move_up(board)
-        elif move == 'a':
-            board, changed = move_left(board)
-        elif move == 'd':
-            board, changed = move_right(board)    
+            if any(2048 in row for row in self.board):
+                print('\nYou win!')
+                break
+
+    def insertNewNumber(self, board):
+        if any(0 in row for row in board):
+            self.board = board
+            loc = [randint(0,3), randint(0,3)]
+            while self.board[loc[0]][loc[1]] != 0:
+                loc = [randint(0,3), randint(0,3)]
+            if random() <= 0.1:
+                self.board[loc[0]][loc[1]] = 4
+            else:
+                self.board[loc[0]][loc[1]] = 2
+
+    def turn(self):
+        moves = {
+            'w' : self.move_up(self.board[:]),
+            'a' : self.move_left(self.board[:]), 
+            's' : self.move_down(self.board[:]),
+            'd' : self.move_right(self.board[:])
+        }
         
-    board = insertNewNumber(board)
-    return board
+        move = 'x'
+        while move not in moves.keys():
+            move = str(input('Enter move: '))
+        
+        if all(self.board==move_board for move_board in moves.values()):
+            self.board = 0
+        else:
+            self.insertNewNumber(moves[move])
 
-def move_down(board):
-    board = transpose(board)
-    board, changed = move_right(board)
-    board = transpose(board)
-    return (board, changed)
+    def move_down(self, copy):
+        return self.transpose(self.move_right(self.transpose(copy)))
 
-def move_up(board):
-    board = transpose(board)
-    board, changed = move_left(board)
-    board = transpose(board)
-    return (board, changed)
-    
-def move_left(board):
-    changed = False
-    for row in range(4):
-        nums = [num for num in board[row] if num != 0]
-        i = 0
-        merged_nums = []
-        while i < len(nums):
-            if i < len(nums)-1 and nums[i] == nums[i+1]:
-                merged_nums.append(2*nums[i])
-                i += 2
-            else:
-                merged_nums.append(nums[i])
-                i += 1
-        merged_nums = merged_nums + [0] * (4-len(merged_nums))
-        if board[row] != merged_nums: 
-            changed = True
-            board[row] = merged_nums
-    return (board, changed)
+    def move_up(self, copy):
+        return self.transpose(self.move_left(self.transpose(copy)))
+        
+    def move_left(self, copy):
+        for row in range(4):
+            nums = [num for num in copy[row] if num != 0]
+            i = 0
+            merged_nums = []
+            while i < len(nums):
+                if i < len(nums)-1 and nums[i] == nums[i+1]:
+                    merged_nums.append(2*nums[i])
+                    i += 2
+                else:
+                    merged_nums.append(nums[i])
+                    i += 1
+            merged_nums = merged_nums + [0] * (4-len(merged_nums))
+            copy[row] = merged_nums
+        return copy
 
-def move_right(board):
-    changed = False
-    for row in range(4):
-        nums = [num for num in board[row] if num != 0]
-        i = len(nums)-1
-        merged_nums = []
-        while i >= 0:
-            if i > 0 and nums[i] == nums[i-1]:
-                merged_nums.insert(0,2*nums[i])
-                i -= 2
-            else:
-                merged_nums.insert(0,nums[i])
-                i -= 1
-        merged_nums = [0] * (4-len(merged_nums)) + merged_nums
-        if board[row] != merged_nums: 
-            changed = True
-            board[row] = merged_nums
-    return (board, changed)
+    def move_right(self, copy):
+        for row in range(4):
+            nums = [num for num in copy[row] if num != 0]
+            i = len(nums)-1
+            merged_nums = []
+            while i >= 0:
+                if i > 0 and nums[i] == nums[i-1]:
+                    merged_nums.insert(0,2*nums[i])
+                    i -= 2
+                else:
+                    merged_nums.insert(0,nums[i])
+                    i -= 1
+            merged_nums = [0] * (4-len(merged_nums)) + merged_nums
+            copy[row] = merged_nums
+        return copy
 
-def transpose(board):
-    return [[board_row[col] for board_row in board] for col in range(4)]
+    def transpose(self, copy):
+        return [[board_row[col] for board_row in copy] for col in range(4)]
 
-board = createBoard()
-print(*board, sep = '\n')
-print()
-game(board)
+Game_2048().game()
